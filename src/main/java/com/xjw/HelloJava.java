5e742f381fdcd5a0730e4d7d986bca391aa3e4d5
xjw111 <1270210514@qq.com>
2022-08-18 20:45:58 +0800

    third commit

--- src/main/java/com/xjw/HelloJava.java
+++ src/main/java/com/xjw/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("你好，世界");
         System.out.println("你好，世界1");
+        System.out.println("你好，世界3");
     }
 }
