1590ffedb3ea06ad06963e51f32b98e3ca0d2f8c
xjw10101 <1270210514@qq.com>
2022-08-19 11:35:01 +0800

    hot-fix commit second

--- src/main/java/com/xjw/HelloJava.java
+++ src/main/java/com/xjw/HelloJava.java
@@ -16,5 +16,6 @@ public class HelloJava {
         System.out.println("你好，master test4");
         System.out.println("你好，hot-fix test3");
         System.out.println("你好，hot-fix test1");
+        System.out.println("你好，hot-fix test2");
     }
 }
